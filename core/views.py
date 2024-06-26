25bb45c95c3f05fd877875ccd2fa279eab14c6a8
thoriqwildan <wildanthoriq14@gmail.com>
2024-06-26 14:08:09 +0700

    added log in feature

--- core/views.py
+++ core/views.py
@@ -1,6 +1,22 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
 
 # Create your views here.
 def home(request):
-    return HttpResponse('hello World')
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            print('error login')
+            return redirect('home')
+    else:
+        return render(request, 'home.html')
+    
+def logout_user(request):
+    logout(request)
+    return redirect('home')
